062ce6777a3a2ae3e4347043a36c9fe35653c8f6
Vladislav Alexeyev <carlopuppy@gmail.com>
2014-11-24 16:27:08 +0400

    Functional

--- prg.sh
+++ prg.sh
@@ -6,30 +6,40 @@
 #SYNTAX: prg_find [path]
 #endif
 
+
+
+# Terminates program if input date is incorrect
+function isDate
+{
+	# MAC OS X: 
+	#date "+%d/%m/%Y" -d "$d/$m/$y" 2>1 > /dev/null
+	#Other: 
+	date -f "%m/%d/%Y" -j "$d/$m/$y" >/dev/null 2>&1
+	is_valid=$?
+	echo $is_valid
+	if [ $is_valid -eq 1 ]
+	then
+		echo "Incorrect date. Program terminated!"
+		exit 1
+	fi
+}
+
 #Enter dates to search
 
 oldIFS="$IFS"
 IFS=":"
 
-
 #Data write
-echo -n "Enter date (format dd:mm:yyyy) "
+echo "Enter date (format dd:mm:yyyy)"
 read d m y
-
-echo $d
-echo $m 
-echo $y
-
-if test -z $d || test -z $m || test -z $y
-then
-echo "Failed date. Exited "
-exit 1
-fi
+isDate d m y
 
 IFS="oldifs"
 
 #Find
-tmp="/root -${RANDOM}${RANDOM}"
+#tmp="/root -${RANDOM}${RANDOM}"
+tmp = "/"
+
 touch -t "${y}${m}${d}0000" "tmp"
 
 a=$(find $(pwd) -type f -newer "$tmp" -print)
